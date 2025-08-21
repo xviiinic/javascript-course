261fe6b7a00a1ca04fd17757ac04bdd88db72818
xviiinix <mmiinix@gmail.com>
2025-08-21 22:05:40 +0800

    feat: add basic variables and data types
    - Set up JavaScript environment and browser console
    - Implement let, const, var variable declarations
    - Add data type exploration with typeof operator
    - Establish development workflow and best practices

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,73 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
+
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("=== VARIABLES ===");
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let age = 30;
+age = 31; // Reassigning the variable
+console.log(age);
+
+const birthYear = 1991;
+console.log(birthYear);
+
+const country = "Portugal";
+const language = "Portuguese";
+const population = 10;
+
+age = 26
+
+firstName = "Sarah";
+let myCurrentJob ="teacher";
+let PI = 3.14159;
+
+console.log("=== DATA TYPES ===");
+
+newAge = 23;
+console.log(newAge);
+console.log(typeof newAge);
+
+firstName = "Jonas";
+console.log(firstName);
+console.log(typeof firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+let dynamicVariable = 23;
+console.log(dynamicVariable, typeof dynamicVariable);
+
+dynamicVariable = "Now I'm a string!";
+console.log(dynamicVariable, typeof dynamicVariable);
+
+dynamicVariable = true;
+console.log(dynamicVariable, typeof dynamicVariable);  
+
+const myName = "Nicole";
+let myAge = 22;
+const myBirthYear = 2002;
+const favNumber = 18;
+iLikeJavaScript = true;
+
+console.log(myName, myAge, myBirthYear, favNumber, iLikeJavaScript);
+console.log(typeof myName, typeof myAge, typeof myBirthYear, typeof favNumber, typeof iLikeJavaScript);
+
+let score = 100;
+console.log("Initial score:", score);
+score = 150;
+console.log("Updated score:", score);
+
+console.log(typeof 42);
+console.log(typeof "Hello");
+console.log(typeof true);
+console.log(typeof undefined);
