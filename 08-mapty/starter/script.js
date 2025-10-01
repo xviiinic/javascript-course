f7ca10114da7f62353ab32771b5a8279c3a9049b
xviiinix <mmiinix@gmail.com>
2025-10-01 12:29:00 +0800

    feat(mapty): implement OOP foundation with Workout class hierarchy
    - Create base Workout class with common properties and methods
    - Implement Running class with cadence tracking and pace calculation
    - Implement Cycling class with elevation gain and speed calculation
    - Add inheritance using extends and super() keywords
    - Include automatic description generation and click tracking
    - Test complete class hierarchy with verification examples
    - Establish solid OOP foundation for Mapty application architecture

--- 08-mapty/starter/script.js
+++ 08-mapty/starter/script.js
@@ -1,1 +1,91 @@
 'use strict';
+
+console.log('Mapty OOP Foundation');
+
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  clicks = 0;
+
+  constructor(coords, distance, duration) {
+    // store the coordinates as an array of lat and long
+    this.coords = coords;
+    // store distance in kilometes
+    this.distance = distance;
+    // store duration in minutes
+    this.duration = duration;
+  }
+
+  // generate workout description
+  _setDescription() {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+      months[this.date.getMonth()]
+    } ${this.date.getDate()}`;
+  }
+
+  click() {
+    this.clicks++;
+  }
+}
+
+const testWorkout = new Workout([40.7128, -74.006], 5.2, 24);
+console.log('Test workout:', testWorkout);
+
+class Running extends Workout {
+  type = 'running';
+
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+    this._setDescription();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  type = 'cycling';
+
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+    this._setDescription();
+  }
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+const run1 = new Running([39.7392, -104.9903], 5.2, 24, 178);
+console.log('Running workout:', run1);
+console.log('Running pace:', run1.pace.toFixed(1), 'min/kim');
+
+const cycling1 = new Cycling([39.7392, -104.9903], 27, 95, 523);
+console.log('Cycling workout:', cycling1);
+console.log('Cycling speed:', cycling1.speed.toFixed(1), 'km/h');
+
+run1.click();
+cycling1.click();
+console.log('Run clicks:', run1.clicks);
+console.log('Cycling clicks:', cycling1.clicks);
